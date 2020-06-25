40742567970037e1f4be43620de12abb35bf2f92
aroldanr <aldorm6000@gmail.com>
2020-06-25 14:33:54 -0600

    version se agrega server api para login

--- src/app/Pages/login/login.page.ts
+++ src/app/Pages/login/login.page.ts
@@ -1,5 +1,8 @@
+import { AuthConstants } from './../../config/auth-constants';
+import { StorageService } from './../../services/storage.service';
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd, NavigationError } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,13 +11,50 @@ import { Router, NavigationEnd, NavigationError } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private router: Router) { }
+  public postData = {
+    username: '',
+    password: ''
 
-  ngOnInit() {
+  };
+
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private StorageService: StorageService
+  ) { }
+
+  ngOnInit() { }
+
+  validateInputs() {
+    let username = this.postData.username.trim();
+    let password = this.postData.password.trim();
+
+    return (this.postData.username &&
+      this.postData.password &&
+      username.length > 0 &&
+      password.length > 0
+    );
   }
 
-  login(){
-    this.router.navigate(['/home/feed'])
+  loginAction() {
+   
+    if (this.validateInputs()) {
+      this.authService.login(this.postData).subscribe((res: any) => {
+        if (res.userData) {
+          this.StorageService.store(AuthConstants.AUTH, res.userData);
+          this.router.navigate(['home']);
+        } else {
+          console.log('Incorrect username or Password');
+        }
+      },
+        (error: any) => {
+          console.log('Network connection error');
+        }
+      );
+    } else {
+      console.log('Please give some information');
+    } 
   }
-
 }
+
+
